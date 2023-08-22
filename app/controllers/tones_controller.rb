4242f616de8f0d1386ceadcf2b755c7e9d196b01
Dimitri Zimermann <dimitrizimer@gmail.com>
2023-08-22 12:35:05 -0300

    work on controllers

--- app/controllers/tones_controller.rb
+++ app/controllers/tones_controller.rb
@@ -1,2 +1,49 @@
 class TonesController < ApplicationController
+
+  # before_action :authenticate_user!
+  before_action :brand, only: [:index, :create]
+
+  def index
+    @tones = @brand.tones.order(created_at: :desc)
+  end
+
+  def create
+    @tone = @brand.tones.new(tone_params)
+    if @tone.save
+      render json: @tone, status: :created
+    else
+      render json: { errors: @tone.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @tone
+  end
+
+  def update
+    if @tone.update(tone_params)
+      render json: @tone
+    else
+      render json: { errors: @tone.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @tone.destroy
+    head :no_content
+  end
+
+  private
+
+  def brand
+    @brand ||= Brand.find(params[:brand_id])
+  end
+
+  def set_tone
+    @tone = Tone.find(params[:id])
+  end
+
+  def tone_params
+    params.require(:tone).permit(:name, :description)
+  end
 end
